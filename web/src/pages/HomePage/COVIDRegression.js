8c89b7e4f2009aa81e3f6d3b71743b26b6dbbead
Phuoc Do <dnprock@gmail.com>
2020-08-14 16:25:46 -0700

    Add comments

--- web/src/pages/HomePage/COVIDRegression.js
+++ web/src/pages/HomePage/COVIDRegression.js
@@ -3,6 +3,8 @@ import Vida from 'vidajs'
 import { timeFormat, timeParse } from 'd3-time-format'
 
 class COVIDRegression extends React.Component {
+  // initialize state in constructor
+  // we'll update this state when we finish loading data
   constructor(props) {
     super(props)
     this.state = {
@@ -11,7 +13,9 @@ class COVIDRegression extends React.Component {
   }
 
   calculateRegression(json) {
+    // we'll format time into UNIX timestamp
     let tf = timeFormat("%s")
+    // this is the input time format
     let tp = timeParse("%Y%m%d")
     let x = json.map(d => +tf(tp(d.date)))
     let y = json.map(d => d.positiveIncrease)
@@ -21,14 +25,17 @@ class COVIDRegression extends React.Component {
     json.map((d, i) => {
       d.predictPositiveIncrease = predictY[i]
     })
-    console.log(json)
   }
 
   componentDidMount() {
+    // fetch data from the COVID Tracking Project
     fetch('https://covidtracking.com/api/v1/us/daily.json')
       .then(async (response) => {
         let json = await response.json()
         json = json.reverse()
+        // filter out data prior to March 01, 2020
+        // COVID testing did not start before,
+        // so the numbers are very low
         json = json.filter(d => d.date > 20200301)
         this.calculateRegression(json)
         this.setState({
